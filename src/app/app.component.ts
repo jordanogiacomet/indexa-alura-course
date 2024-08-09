0c1a9a886ce13e389d812f1c9e91d7e891c2eb6b
jordanogiacomet <110415222+jordanogiacomet@users.noreply.github.com>
2024-08-09 10:06:07 -0300

    WIP - Indexa project Angular Frontend;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from "./components/container/container.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { SeparatorComponent } from "./components/separator/separator.component";
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContainerComponent, HeaderComponent, SeparatorComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto : string = 'abcdefghijklmnopqrstuvwxyz';
 }
