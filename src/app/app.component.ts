ca24d51be208e89255829f26a8a858ef6e5d8a1b
jordanogiacomet <110415222+jordanogiacomet@users.noreply.github.com>
2024-08-09 11:08:36 -0300

    WIP - Aula 4 completa;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,33 @@ import { ContainerComponent } from "./components/container/container.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { SeparatorComponent } from "./components/separator/separator.component";
 import { ContactComponent } from './components/contact/contact.component';
+import { FormsModule } from '@angular/forms'
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from './agenda.json';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent, HeaderComponent, SeparatorComponent, ContactComponent],
+  imports: [RouterOutlet, ContainerComponent, HeaderComponent, SeparatorComponent, ContactComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   alfabeto : string = 'abcdefghijklmnopqrstuvwxyz';
-}
+  contatos: Contato[] = agenda;
+
+  filtroPorTexto: string = '';
+
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.contatos.filter(contato => contato.nome.toLowerCase().startsWith(letra.toLowerCase()));
+    };
+  };
+
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  @Input() nome: string  = '';
+  @Input() telefone: string = '';
 }
